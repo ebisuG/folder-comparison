92bbfa436c0f7d5c9f9b4633dbba380ee4db9406
Eiichi Sayama <eiichimouse@outlook.jp>
2025-03-23 22:37:03 +0900

    test:test for receiveArguments

--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -21,4 +23,39 @@ func Test_doesExist(t *testing.T) {
 	if err != nil {
 		fmt.Printf("remove failed : %v", err)
 	}
+}
+
+func Test_receiveArguments(t *testing.T) {
+	os.Mkdir("./folder1-1", 0777)
+	os.Mkdir("folder1-2", 0777)
+	defer os.RemoveAll("./folder1-1/")
+	defer os.RemoveAll("./folder1-2/")
+	correctInput1 := "./folder1-1 folder1-2"
+	wrongInput1 := ""
+	wrongInput2 := " ./wrongFile2-1"
+	wrongTestInputs := []string{wrongInput1, wrongInput2}
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+
+	args := strings.Split(correctInput1, " ")
+	fmt.Println(args)
+	result, err := cli.receiveArguments(args)
+	if err != nil {
+		t.Errorf("return value should be nil : %v \n", err)
+	}
+	if !(result[0] == "./folder1-1" && result[1] == "folder1-2") {
+		t.Errorf("length of return value should be two : %v \n", err)
+	}
+
+	for _, v := range wrongTestInputs {
+		args := strings.Split(v, " ")
+		result, err := cli.receiveArguments(args)
+		if err == nil {
+			t.Errorf("receiveArguments should fail : %v \n", err)
+		}
+		if len(result) != 0 {
+			t.Errorf("receiveArguments should return empty array : %v \n", err)
+		}
+	}
+
 }
